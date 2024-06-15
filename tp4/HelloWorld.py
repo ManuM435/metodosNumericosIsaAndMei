edd271564293b7a566bb8996e47e8f1efe87ca66
castaisa <icastaneda@udesa.edu.ar>
2024-06-15 18:15:53 -0300

    mei es un panchito y se hace GD con el step 1/lambdamax pero etoy forzando que no sea imaginario

--- tp4/HelloWorld.py
+++ tp4/HelloWorld.py
@@ -1,5 +1,7 @@
 import numpy as np
 
+np.random.seed(9923)
+
 def gradient(A, x, b):
     return 2 * A.T @ (A @ x - b)
 
@@ -11,8 +13,29 @@ def gradient_descent(start, A, b, learn_rate, iters):
         x += diff
     return x.tolist()
 
-A = np.array([[1, 2], [3, 4]])
-b = np.array([1, 2])
+n = 5
+d = 100
 
-iterations = 3000000
-print(gradient_descent([1, 1], A, b, 0.01, iterations))
+# Generar una matriz A de tamaño n x d con valores aleatorios
+A = np.random.rand(n, d)
+
+# Generar un vector b de tamaño n con valores aleatorios
+b = np.random.rand(n)
+
+# Inicializar el vector x con d elementos
+start = np.random.rand(d)
+
+#Calcula el step 1/lambda max que es el maximo autovalor del hessiano de F(x), AtA
+hessiano = A.T @ A
+autovalores_h = np.linalg.eigvals(hessiano)
+
+# Toma la parte real de los autovalores
+autovalores_reales = np.real(autovalores_h)
+
+
+step = 1/max(autovalores_reales)
+print(step)
+
+iterations = 300
+print(gradient_descent(start, A, b, step, iterations))
+print(len(gradient_descent(start, A, b, step, iterations)))
